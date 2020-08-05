1f1194c753b96977f3097fa196bffc9de72f37d3
Nebir Kiron <kironnibir046@gmail.com>
2020-08-05 20:34:12 +0600

    updated contacy page and email system

--- dental/settings.py
+++ dental/settings.py
@@ -123,3 +123,21 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
+
+
+
+"""
+EMAIL_HOST = 'smtp.google.com' # if not work replace google by gmail
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dr@gmail.com'
+EMAIL_HOST_PASSWORD = 'drpassword'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+"""
+
+#  python -m smtpd -n -c DebuggingServer localhost:1025
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
--- website/views.py
+++ website/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 def home(request):
     return render(request,'home.html',{})
+
+def contact(request):
+    if request.method == "POST":        
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+        
+        #send mail
+
+        send_mail(
+        message_name,
+        message,
+        message_email,
+        ['kironnibir046@gmail.com'],
+        fail_silently=False,
+        )
+
+        return render(request,'contact.html',{'name':message_name,})
+    else:
+        return render(request,'contact.html',{})
+    
+    
